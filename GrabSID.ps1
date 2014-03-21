22d0fabc4dc8e69b42519625c0121dd270b91680
Brian Gonzalez <brianfgonzalez@gmail.com>
2014-03-21 11:47:20 -0400

    Updated MDT Scripting Templates

---
+++ GrabSID.ps1
@@ -0,0 +1,3 @@
+﻿$sLockedDownSID = (gwmi -Query "SELECT SID From Win32_UserAccount WHERE Name LIKE ""Locked%""").SID
+$sSupportSID = (gwmi -Query "SELECT SID From Win32_UserAccount WHERE Name LIKE ""Locked%""").SID
+Write-Host $sLockedDownSID
---
+++ ReplacePartOfFileNameWithSID.ps1
@@ -0,0 +1,4 @@
+﻿$sPictureFolderPath = "C:\AiO\Applications\Assign Wallpaper and User Tile\Support"
+$sBrianDownSID = (gwmi -Query "SELECT SID From Win32_UserAccount WHERE Name LIKE ""Brian%""").SID
+Get-ChildItem $sPictureFolderPath -Filter "*.jpg" | `
+    fl *
