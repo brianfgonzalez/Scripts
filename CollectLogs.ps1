8d1d26eb184261e7b4bfbea3df5fd4e1f80896c9
Brian Gonzalez <brianfgonzalez@gmail.com>
2015-09-11 10:11:42 -0400

    Added Capture Logs script

---
+++ CollectLogs.ps1
@@ -0,0 +1,144 @@
+<#
+Version: 1.1
+ChangeLog:
+	- September 10, 2015 : First revision of script.
+    - Moved function code above function call.
+#>
+function fReRunPrompt{
+$sRet = [Microsoft.VisualBasic.Interaction]::MsgBox("Would you like to rerun the script?",'YesNo,Question', "Rerun script prompt")
+If ($sRet -eq "Yes")
+{
+    fMain
+}
+}
+
+
+function fMain
+{
+
+#$ErrorActionPreference = "SilentlyContinue"
+#trap {"Error found: $_" | Out-File "F:\CollectLogs.log"}
+
+# Locate USB media drive letter
+#######################################################################################################################################################
+try
+{
+    $oAllUSBDrive = (Get-WmiObject -Query "SELECT * FROM Win32_LogicalDisk WHERE DriveType LIKE 2")[0]
+    if ($oAllUSBDrive -eq $null)
+    {
+        $oAllUSBDrive = (Get-WmiObject -Query "SELECT * FROM Win32_LogicalDisk WHERE DriveType LIKE 2")
+    }
+    $sUSBDriveLetter = $oAllUSBDrive.Name
+}
+catch
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.VisualBasic") | Out-Null
+    [Microsoft.VisualBasic.Interaction]::MsgBox("An error occurred when attempting to locate USB drive.",'OKOnly,Exclamation', "Error encountered") | Out-Null
+    fReRunPrompt
+    Exit
+}
+
+# Locate Local HDD drive letter (100GB+)
+#######################################################################################################################################################
+try
+{
+    $oLocalLargestHDD = (Get-WmiObject -Query "SELECT * FROM Win32_LogicalDisk WHERE DriveType LIKE 3" | Sort-Object -Descending Size)[0]
+    If ($oLocalLargestHDD.Size -gt 100000000)
+    {
+        $sHDDDriveLetter = $oLocalLargestHDD.Name
+    } else {
+        [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.VisualBasic") | Out-Null
+        [Microsoft.VisualBasic.Interaction]::MsgBox("HDD is NOT 100GB, so there is no SYSTEM partition!",'OKOnly,Exclamation', "Error encountered") | Out-Null
+        fReRunPrompt
+        Exit
+    }
+}
+catch
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.VisualBasic") | Out-Null
+    [Microsoft.VisualBasic.Interaction]::MsgBox("An error occurred when attempting to locate a HDD drive.",'OKOnly,Exclamation', "Error encountered") | Out-Null
+    fReRunPrompt
+    Exit
+}
+
+# Create folder on USB media named with DATE and TIME stamp information
+#######################################################################################################################################################
+$sDateFormat = (Get-Date -format "yyyy-M-d_HHmm")
+$sLogFolderPath = $sUSBDriveLetter+"\"+$sDateFormat
+New-Item -type Directory -path $sLogFolderPath -force
+
+# Copy all Windows or Deployment related log files on local HDD
+try
+{
+    $oLocalLargestHDD = (Get-WmiObject -Query "SELECT * FROM Win32_LogicalDisk WHERE DriveType LIKE 3" | Sort-Object -Descending Size)[0]
+    If ($oLocalLargestHDD.Size -gt 100000000)
+    {
+        $sHDDDriveLetter = $oLocalLargestHDD.Name
+    } else {
+        [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.VisualBasic") | Out-Null
+        [Microsoft.VisualBasic.Interaction]::MsgBox("HDD is NOT 100GB, so there is no SYSTEM partition!",'OKOnly,Exclamation', "Error encountered") | Out-Null
+        fReRunPrompt
+        Exit
+    }
+}
+catch
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.VisualBasic") | Out-Null
+    [Microsoft.VisualBasic.Interaction]::MsgBox("An error occurred when attempting to locate a HDD drive.",'OKOnly,Exclamation', "Error encountered") | Out-Null
+    fReRunPrompt
+    Exit
+}
+
+# Copy log files into created Directory on USB drive
+#######################################################################################################################################################
+# Function to reuse search and copy
+function fSearchAndCopy($aDirectories)
+{
+    $sInt=0
+    foreach ($sDirectory in $aDirectories)
+    {
+        if ((Test-Path $sDirectory) -eq $true)
+        {
+            $sInt = $sInt + 1
+            $sDestinationDirectory = "$sLogFolderPath\$sInt"
+            Write-Host $sDirectory
+            New-Item -type Directory -path "$sDestinationDirectory" -force
+            Copy-Item -Path "$sDirectory\*" -Destination "$sDestinationDirectory" -Force -Recurse
+            Add-Content "$sDestinationDirectory\OriginalDirectory.txt" $sDirectory
+        }
+    }
+}
+
+# Populate Directories Array
+$aDirectories =
+    @("$sHDDDriveLetter\Windows\Temp\DeploymentLogs",
+    "$sHDDDriveLetter\MININT\SMSOSD\OSDLOGS",
+    "$sHDDDriveLetter\Windows\System32\sysprep\Panther",
+    "$sHDDDriveLetter\Windows\Panther",
+    "$sHDDDriveLetter\_SMSTaskSequence",
+    "$sHDDDriveLetter\SMSTSLog")
+
+#Initiate folder copy routine
+try
+{
+    fSearchAndCopy($aDirectories)
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.VisualBasic") | Out-Null
+    [Microsoft.VisualBasic.Interaction]::MsgBox("Copy of content is complete.  Shutting down unit.",'OKOnly,Information', "Error encountered") | Out-Null
+    Start-Process "x:\windows\system32\wpeutil.exe" @('"shutdown"')
+}
+catch
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.VisualBasic") | Out-Null
+    [Microsoft.VisualBasic.Interaction]::MsgBox("Error occured during the copy of the deployment files.",'OKOnly,Exclamation', "Error encountered") | Out-Null
+    fReRunPrompt
+    Exit
+}
+
+
+}
+
+
+
+
+# Call fMain
+fMain
